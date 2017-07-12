f55e9e2861484b5818533f1346f52e6216f79f43
Simon Gaestel <simon.gaestel+git@gmail.com>
2017-07-12 17:16:41 +0200

    PLTFRS-8006 - Format with prettier (#1)
    * PLTFRS-8006 - Use prettier as formatter and eslint as linter
    
    * PLTFRS-8006 - Format with prettier using 2 spaces tabs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,22 @@
 module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-release');
+  grunt.loadNpmTasks('grunt-tag');
 
-    grunt.loadNpmTasks('grunt-release');
-    grunt.loadNpmTasks('grunt-tag');
-
-    grunt.initConfig({
-        release: {
-            options: {
-                npm: false,
-                afterReleaseTasks: ['tag'],
-                github: {
-                    repo: "AirVantage/node-tech-time",
-                    accessTokenVar: "GITHUB_ACCESS_TOKEN"
-                }
-            }
-        },
-        tag: {
-            options: {
-                tagName: '<%= version.match(/\\d*/) %>.x'
-            }
+  grunt.initConfig({
+    release: {
+      options: {
+        npm: false,
+        afterReleaseTasks: ['tag'],
+        github: {
+          repo: 'AirVantage/node-tech-time',
+          accessTokenVar: 'GITHUB_ACCESS_TOKEN'
         }
-    });
+      }
+    },
+    tag: {
+      options: {
+        tagName: '<%= version.match(/\\d*/) %>.x'
+      }
+    }
+  });
 };
